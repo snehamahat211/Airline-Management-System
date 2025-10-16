fbfffc0c79ce6eb0b3975c1ee70965c42998999b
snehamahat211 <snehamahat211@gmail.com>
2025-10-16 13:18:50 +0545

    readme added

---
+++ src/Login.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Login extends JFrame {
+    public Login(){
+        getContentPane().setBackground(Color.WHITE);
+        setSize(400,250);
+        setLocation(600,250);
+        setVisible(true);
+
+
+
+
+
+
+    }
+    public static void main (String[] args){
+        new Login();
+
+    }
+}
