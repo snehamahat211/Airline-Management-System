b92dc4d3f0e7b9988bad4e50aa65c9e918971c3d
snehamahat211 <snehamahat211@gmail.com>
2025-10-21 21:09:55 +0545

    form added

--- src/AddCustomer.java
+++ src/AddCustomer.java
@@ -3,7 +3,7 @@ import java.awt.*;
 
 
 public class AddCustomer extends JFrame {
-    JTextField nameTextField;
+    JTextField nameTextField,nationfield,citizenfield;
     AddCustomer (){
         getContentPane().setBackground(Color.WHITE);
         setSize(650, 650);
@@ -16,14 +16,35 @@ public class AddCustomer extends JFrame {
         add(l1);
 
         JLabel name = new JLabel("Name:");
-        name.setFont(new Font("Arial", Font.BOLD, 20));
-        name.setBounds(80, 140, 100, 30);   //y=height of particular case +y axis
+        name.setFont(new Font("Arial", Font.BOLD, 15));
+        name.setBounds(80, 140, 100, 20);
         add(name);
 
         nameTextField = new JTextField();
         nameTextField.setFont(new Font("Arial", Font.BOLD, 20));
-        nameTextField.setBounds(190, 140, 220, 30);
+        nameTextField.setBounds(210, 140, 250, 30);
         add(nameTextField);
+
+        JLabel nationality = new JLabel("Nationality:");
+        nationality.setFont(new Font("Arial", Font.BOLD, 15));
+        nationality.setBounds(80, 190, 100, 20);   //y=height of particular case +y axis
+        add(nationality);
+
+        nationfield= new JTextField();
+        nationfield.setFont(new Font("Arial", Font.BOLD, 20));
+        nationfield.setBounds(210, 190, 250, 30);
+        add(nationfield);
+
+        JLabel Citizenship = new JLabel("Citizenship no:");
+        Citizenship.setFont(new Font("Arial", Font.BOLD, 15));
+        Citizenship.setBounds(80, 240, 150, 20);
+        add(Citizenship);
+
+        citizenfield= new JTextField();
+        citizenfield.setFont(new Font("Arial", Font.BOLD, 20));
+        citizenfield.setBounds(210, 240, 250, 30);
+        add(citizenfield);
+
 
 
 
