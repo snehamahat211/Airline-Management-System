925d68c2fd5a145c245d6d401b9b6ad6e4eadc15
snehamahat211 <snehamahat211@gmail.com>
2025-11-03 10:02:54 +0545

    set the textfield and buttons

--- src/JourneyDetails.java
+++ src/JourneyDetails.java
@@ -6,9 +6,34 @@ import java.sql.*;
 import net.proteanit.sql.DbUtils;
 public class JourneyDetails extends JFrame implements ActionListener {
     JTable table;
+    JTextField pnrfield;
+    JButton show;
+
     public JourneyDetails() {
         getContentPane().setBackground(Color.WHITE);
         setLayout(null);
+
+        JLabel pnr= new JLabel("PNR:");
+        pnr.setForeground(Color.BLACK);
+        pnr.setFont(new Font("Times New Roman", Font.BOLD, 20));
+        pnr.setBounds(20,15,80,20);
+        add(pnr);
+
+        pnrfield=new JTextField();
+        pnrfield.setBounds(70,10,160,30);
+        add(pnrfield);
+
+
+        show=new JButton("Show");
+        show.setBackground(Color.BLACK);
+        show.setForeground(Color.WHITE);
+        show.setFont(new Font("Times New Roman", Font.BOLD, 20));
+        show.setBounds(280,10,100,30);
+        add(show);
+
+
+
+
 
 
 
