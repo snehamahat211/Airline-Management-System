333e05df11df403f159bdabd56f0f472ea9febe2
snehamahat211 <snehamahat211@gmail.com>
2025-11-27 11:51:14 +0545

    fetchingb done

--- src/CancelFlight.java
+++ src/CancelFlight.java
@@ -4,24 +4,21 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.ResultSet;
 import com.toedter.calendar.JDateChooser;
-import  java.util.*;
+import java.util.*;
 
 public class CancelFlight extends JFrame implements ActionListener {
+
     JTextField pnrfield;
     JButton fetch, flight;
     JDateChooser dcdate;
 
-
-    JLabel name, pnr, Flightc,cancelt,dot,canceltext;
+    JLabel name, pnr, Flightc, cancelt, dot, canceltext;
 
     CancelFlight() {
 
         setLayout(null);
         JLabel heading = new JLabel("CANCELLATION");
-
-        Random random =new Random ();
-
-        heading.setBounds(450, 50, 500, 35);
+        heading.setBounds(250, 50, 500, 35);
         heading.setFont(new Font("Times New Roman", Font.BOLD, 20));
         add(heading);
 
@@ -32,13 +29,13 @@ public class CancelFlight extends JFrame implements ActionListener {
 
         pnrfield = new JTextField();
         pnrfield.setFont(new Font("Arial", Font.BOLD, 20));
-        pnrfield.setBounds(210, 140, 250, 30);
+        pnrfield.setBounds(210, 140, 200, 30);
         add(pnrfield);
 
         fetch = new JButton("Fetch");
         fetch.setBackground(Color.RED);
         fetch.setForeground(Color.WHITE);
-        fetch.setBounds(480, 140, 130, 30);
+        fetch.setBounds(420, 140, 130, 30);
         fetch.addActionListener(this);
         add(fetch);
 
@@ -47,51 +44,62 @@ public class CancelFlight extends JFrame implements ActionListener {
         name.setBounds(80, 190, 500, 20);
         add(name);
 
-
         cancelt = new JLabel("Cancel Ticket:");
         cancelt.setFont(new Font("Arial", Font.BOLD, 15));
-        cancelt.setBounds(80, 540, 300, 20);
+        cancelt.setBounds(80, 240, 300, 20);
         add(cancelt);
 
-        canceltext = new JLabel(""+random.nextInt(1000000));
+        canceltext = new JLabel("" + new Random().nextInt(1000000));
         canceltext.setFont(new Font("Arial", Font.BOLD, 15));
-        canceltext.setBounds(120, 540, 300, 20);
+        canceltext.setBounds(200, 240, 300, 20);
         add(canceltext);
-
-
 
         Flightc = new JLabel("Flight Code:");
         Flightc.setFont(new Font("Arial", Font.BOLD, 15));
-        Flightc.setBounds(80, 540, 300, 20);
+        Flightc.setBounds(80, 290, 300, 20);
         add(Flightc);
 
         dot = new JLabel("Date:");
         dot.setFont(new Font("Arial", Font.BOLD, 15));
-        dot.setBounds(80, 590, 150, 20);
+        dot.setBounds(80, 340, 150, 20);
         add(dot);
 
-        dcdate = new JDateChooser();
-        dcdate.setBounds(200, 590, 200, 25);
-        add(dcdate);
 
         flight = new JButton("Cancel");
         flight.setBackground(Color.GREEN);
         flight.setForeground(Color.BLACK);
-        flight.setBounds(480, 640, 130, 30);
+        flight.setBounds(380, 380, 130, 30);
         flight.addActionListener(this);
         add(flight);
 
         getContentPane().setBackground(Color.WHITE);
-        setSize(1100, 760);
+        setSize(800, 460);
         setLocation(100, 30);
         setVisible(true);
     }
 
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == fetch) {
+            try {
+                String pnrValue = pnrfield.getText();
+                Conn c = new Conn();
+                String query = "select * from reservation where pnr = '" + pnrValue + "'";
+                ResultSet rs = c.s.executeQuery(query);
 
+                if (rs.next()) {
+                    name.setText("Name: " + rs.getString("name"));
+                    Flightc.setText("Flight Code: " + rs.getString("flight_code"));
+                    dot.setText("Date: " + rs.getString("Date_of_travel"));
+                } else {
+                    JOptionPane.showMessageDialog(null, "No record found");
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
 
-
-    public static void main (String[]args){
+    public static void main(String[] args) {
         new CancelFlight();
     }
 }
-
